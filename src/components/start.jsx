13d5601abdf42be2155916ab99491e48a961d04a
charles ayomike <cayomike@gmail.com>
2022-12-03 22:11:00 -1200

    minor update

--- src/components/start.jsx
+++ src/components/start.jsx
@@ -3,13 +3,13 @@ import {Link} from 'react-router-dom'
 function Start(){
 	return(
 		<div className='main'>
-			<h2>X and O</h2>
+			<h2>Tic-Tac-Toe</h2>
 
 			<div className='info'>
 				<h3>how to play</h3>
 				<hr/>
 				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut laboreLorem ipsum
+					Tic-Tac-Toe is a simple and fun game for 2 players, X and O. It is played on a 3x3 grid. Each player's goal is to make 3 in a row.
 				</p>
 			</div>
 			<Link to='/board' className='btn'>Start</Link>
